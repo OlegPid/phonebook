da3575b68ed40a0935338c48e0bdc2b09f220943
OlegPid <m0509115356@gmail.com>
2017-04-21 11:37:41 +0300

    db config

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=phonebook',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=us-cdbr-iron-east-03.cleardb.net;dbname=heroku_af51892fb898fad',
+    'username' => 'b19a4f6f22efab',
+    'password' => '4def3d61',
     'charset' => 'utf8',
 ];
